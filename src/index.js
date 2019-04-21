a365b5a207558bf42a7d73431e775b6e1a2b9473
ArturRiyanov <49426563+ArturRiyanov@users.noreply.github.com>
2019-04-21 21:02:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,60 +1,93 @@
 import "babel-polyfill";
 import Chart from "chart.js";
 
-const currencyURL = "www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
-// const meteoURL = "/xml.meteoservice.ru/export/gismeteo/point/140.xml";
+const meteoURL = "xml.meteoservice.ru/export/gismeteo/point/140.xml";
 
-async function loadCurrency() {
-  const response = await fetch(currencyURL);
+async function parseData() {
+  const response = await fetch(meteoURL);
   const xmlTest = await response.text();
   const parser = new DOMParser();
-  const currencyData = parser.parseFromString(xmlTest, "text/xml");
-  // <Cube currency="USD" rate="1.1321" />
-  const rates = currencyData.querySelectorAll("Cube[currency][rate]");
-  const result = Object.create(null);
-  for (let i = 0; i < rates.length; i++) {
-    const rateTag = rates.item(i);
-    const rate = rateTag.getAttribute("rate");
-    const currency = rateTag.getAttribute("currency");
-    result[currency] = rate;
-  }
-  result["EUR"] = 1;
-  // result["RANDOM"] = 1 + Math.random();
-  return result;
-}
+  const weatherData = parser.parseFromString(xmlTest, "text/xml");
+  const hours = weatherData.querySelectorAll("FORECAST[hour]");
+  const temperatures = weatherData.querySelectorAll("TEMPERATURE[max][min]");
+  const heats = weatherData.querySelectorAll("HEAT[max][min]");
 
-function normalizeDataByCurrency(data, currency) {
-  const result = Object.create(null);
-  const value = data[currency];
-  for (const key of Object.keys(data)) {
-    result[key] = value / data[key];
+  const resultTemp = Object.create(null);
+  const resultHeat = Object.create(null);
+  for (let i = 0; i < hours.length; i++) {
+    const HoursTag = hours[i];
+    const hour = HoursTag.getAttribute("hour");
+
+    const TemperaturesTag = temperatures[i];
+    const temperatureMax = parseInt(TemperaturesTag.getAttribute("max"));
+    const temperatureMin = parseInt(TemperaturesTag.getAttribute("min"));
+    const temperature = temperatureMin + (temperatureMax - temperatureMin) / 2;
+
+    const HeatsTag = heats[i];
+    const heatMax = parseInt(HeatsTag.getAttribute("max"));
+    const heatMin = parseInt(HeatsTag.getAttribute("min"));
+    const heat = heatMin + (heatMax - heatMin) / 2;
+
+
+
+
+    
+    resultTemp[hour] = temperature;
+    resultHeat[hour] = heat;
   }
-  return result;
+  return [resultTemp, resultHeat];
 }
 
 const buttonBuild = document.getElementById("btn");
 const canvasCtx = document.getElementById("out").getContext("2d");
 buttonBuild.addEventListener("click", async function() {
-  const currencyData = await loadCurrency();
-  const normalData = normalizeDataByCurrency(currencyData, "RUB");
-  const keys = Object.keys(normalData).sort((k1, k2) =>
-    compare(normalData[k1], normalData[k2])
-  );
-  const plotData = keys.map(key => normalData[key]);
+  const weatherData = await parseData();
+  const keysTemp = Object.keys(weatherData[0]);
+  const keysHeat = Object.keys(weatherData[1]);
+  const tempData = keysTemp.map(key => weatherData[0][key]);
+  const heatData = keysHeat.map(key => weatherData[1][key]);
 
   const chartConfig = {
     type: "line",
 
     data: {
-      labels: keys,
+      labels: keysTemp,
       datasets: [
         {
-          label: "Стоимость валюты в рублях",
-          backgroundColor: "rgb(255, 20, 20)",
-          borderColor: "rgb(180, 0, 0)",
-          data: plotData
+          label: "Температура по ощущениям",
+          fill: true,
+          backgroundColor: "rgb(39, 250, 157, 1)",
+          borderColor: "rgb(63, 238, 174)",
+          data: heatData
+        },
+        {
+          label: "Температура воздуха",
+          fill: true,
+          backgroundColor: "rgb(244, 4, 50, 1)",
+          borderColor: "rgb(253, 36, 72)",
+          data: tempData
         }
       ]
+    },
+    options: {
+      scales: {
+        xAxes: [
+          {
+            scaleLabel: {
+              display: true,
+              labelString: "Время"
+            }
+          }
+        ],
+        yAxes: [
+          {
+            scaleLabel: {
+              display: true,
+              labelString: "Температура°C"
+            }
+          }
+        ]
+      }
     }
   };
 
@@ -69,9 +102,3 @@ buttonBuild.addEventListener("click", async function() {
     window.chart = new Chart(canvasCtx, chartConfig);
   }
 });
-
-function compare(a, b) {
-  if (a > b) return 1;
-  if (a < b) return -1;
-  return 0;
-}
